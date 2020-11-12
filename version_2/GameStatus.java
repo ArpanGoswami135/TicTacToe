a743073b64c3da1d6058c6163253480a22cb0897
ArpanGoswami135 <arpangoswami49@gmail.com>
2020-11-12 12:34:31 +0600

    Update names, added a folder and a text file

--- version_2/GameStatus.java
+++ version_2/GameStatus.java
@@ -3,7 +3,7 @@
  *
  * STUDENTS: Nothing to change here.
  *
- * @author Andrew Vardy
+ * @author Arpan Goswami
  */
 
 public enum GameStatus {
--- version_2/SmartAI.java
+++ version_2/SmartAI.java
@@ -3,7 +3,7 @@
  *
  * STUDENTS: Nothing to change here.
  *
- * @author Andrew Vardy
+ * @author Arpan Goswami
  */
 
 import java.lang.UnsupportedOperationException;
